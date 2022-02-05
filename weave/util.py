41102e244cad9abb33dc6db3b2070241182cd415
Alan Watson <alphawhiskey82@yahoo.com>
2022-02-04 22:21:12 -0500

    Working multi-resolution weave_read implementation

--- weave/util.py
+++ weave/util.py
@@ -42,46 +42,7 @@ def getMetaFile(location):
     return os.path.join(location,'weave.json')
 
 
-def make5DimSlice(key,res,weaveNumber):
-    '''
-    Should take a slice key and return a tuple of slices of 5 dims (t,c,z,y,x)
-    If resolution is specified in the input key by 6 dim (res,t,c,z,y,x) then 
-    resolution will be extracted and returned.
-    '''
-    
-    print('In key: {}'.format(key))
-    # For a complete 6 dim slice, extract resolution level [0] and reform
-    # the key as a 5 dim tuple        
-    if not isinstance(key, (slice,int)) and len(key) == 6:
-        res = key[0]
-        if res >= weaveNumber:
-            raise ValueError('Layer is larger than the number of ResolutionLevels')
-        key = tuple((x for x in key[1::]))
 
-    if isinstance(key, int):
-        key = [slice(key)]
-        
-    # All slices must be converted to 5 dims and placed into a tuple
-    if isinstance(key, slice):
-        key = [key]
-
-    # Convert int/slice mix to a tuple of slices
-    elif isinstance(key, tuple):
-        key = tuple((slice(x) if isinstance(x, (int,list)) else x for x in key))
-    
-    # Size tuple to len==5 by appending slice(None) to the end
-    if len(key) < 5:
-        key = tuple(
-            (key[x] if len(key)>x else slice(None) for x in range(5))
-            )
-    else:
-        tuple(key)
-    
-    print('Out key: {}'.format(key))
-    ## NOTE: At this point, key should always be of len(key)==5 and each
-    ## element should be a slice object.
-    return key,res
-    
 
     
 
--- weave/weave_read.py
+++ weave/weave_read.py
@@ -18,6 +18,7 @@ Created on Mon Jan 31 20:34:30 2022
 
 import os
 import json
+import math
 import numpy as np
 import tifffile
 import imagecodecs
@@ -29,15 +30,40 @@ from dask.delayed import delayed
 from itertools import product
 ## weave specific imports
 try:
-    from .util import pixToMB, MBToPix, prepareAndGetFilePath, getFullFilePath, makeDir, getMetaFile,make5DimSlice
+    from .util import pixToMB, MBToPix, prepareAndGetFilePath, getFullFilePath, makeDir, getMetaFile
 except Exception:
     try:
-        from util import pixToMB, MBToPix, prepareAndGetFilePath, getFullFilePath, makeDir, getMetaFile,make5DimSlice
+        from util import pixToMB, MBToPix, prepareAndGetFilePath, getFullFilePath, makeDir, getMetaFile
     except Exception:
-        from weave.util import pixToMB, MBToPix, prepareAndGetFilePath, getFullFilePath, makeDir, getMetaFile,make5DimSlice
+        from weave.util import pixToMB, MBToPix, prepareAndGetFilePath, getFullFilePath, makeDir, getMetaFile
 
-location= r'/CBI_Hive/globus/pitt/bil/weave'
-# location = r'c:\code\weave_out'
+# location= r'/CBI_Hive/globus/pitt/bil/weave'
+location = r'c:\code\weave_out'
+
+'''
+Currently this is working!
+
+To use:
+    -weave_read(location_of_dataset:str, ResolutionLock=some_int_up_to_weaveNumber)
+    -All arrays are represented as 5-dim (t,c,z,y,x)
+    -Slicing over 5D or fewer dimensions will extract data from the ResolutionLock (default 0)
+    -Slicing over 6D will use dim 0 as the resolution  (res,t,c,z,y,x)
+    -Fancy slicing is not yet implemented
+
+To Do:
+    -Resolution levels only downsample in 2D (y,x).  Need to change this to 3D.
+        -Can implement by making ResolutionLock subsample z-layers
+        -Could implement 3D weave
+        -Could in principle implement n-dimensional weave
+    -Parallelize reads
+    -Is is possible to parallelize placing data onto canvas?
+    
+    -Implement caching of individual tiles so not to duplicate work for high-res reads
+        -Implement RAM and persistant cache
+    -Alternative storage formats?
+    -Cusomizeable compression scheme?
+    
+'''
 
 ## A class to read weave
 class weave_read:
@@ -47,7 +73,6 @@ class weave_read:
         '''
         
         self.location = location
-        self.ResolutionLock = ResolutionLock
         self.metaFile = os.path.join(location,'weave.json')
         
         with open(self.metaFile, 'r') as f:
@@ -58,17 +83,21 @@ class weave_read:
             if isinstance(self.meta[ii],list):
                 self.meta[ii] = tuple(self.meta[ii])
         
-        self.shape = self.meta['shape']
-        self.size = self.meta['size']
+        self.setResolutionLock(ResolutionLock)
         self.chunks = self.meta['chunks']
         self.dtype = np.dtype(self.meta['dtype'])
         self.compression = self.meta['compression']
         self.weaveNumber = self.meta['weaveNumber']
         
+    def setResolutionLock(self,ResolutionLock):
+        self.ResolutionLock = ResolutionLock
+        self.shape = (*self.meta['shape'][:-2],*self.meta['resolution{}_shape'.format(self.ResolutionLock)])
+        self.size = math.prod(self.shape)
+        
     def __getitem__(self,key):
         
         # Force a 5 dim slice representing (t,c,z,y,x)
-        key,res = make5DimSlice(key,self.ResolutionLock,self.weaveNumber)
+        key,res = self.make5DimSlice(key)
         
         # Transform slices to have start,stop,step be completely filled.
         key = self.fillSlice(key)
@@ -77,19 +106,75 @@ class weave_read:
         # the size of resolution leve.  Error if not valid.
         self.isSliceValid(key,res)
         
-        
-        '''
-        NOTE: At this point, key should always be of len(key)==5 and each
-        element should be a slice object.
-        
-        res will indicate the resolution level from which to extract data.
-        '''
-        
-        ##  Need to work on a way to extract 3D accross Z
-        if all(x==slice(None) for x in key[-2::]):
-            pass
-            # return self.getResolutionLevelZ(res,0,0,key) #<-- not correct
+        return self.getSlice(res,key)
                 
+    
+    def make5DimSlice(self,key):
+        '''
+        Should take a slice key and return a tuple of slices of 5 dims (t,c,z,y,x)
+        If resolution is specified in the input key by 6 dim (res,t,c,z,y,x) then 
+        resolution will be extracted and returned.
+        '''
+        
+        print('In key: {}'.format(key))
+        
+        res = self.ResolutionLock
+        # For a complete 6 dim slice, extract resolution level [0] and reform
+        # the key as a 5 dim tuple        
+        if not isinstance(key, (slice,int)) and len(key) == 6:
+            res = key[0]
+            if res >= self.weaveNumber:
+                raise ValueError('Layer is larger than the number of ResolutionLevels')
+            key = tuple((x for x in key[1::]))
+
+        if isinstance(key, int):
+            key = [slice(key)]
+            
+        # All slices must be converted to 5 dims and placed into a tuple
+        if isinstance(key, slice):
+            key = [key]
+
+        # Convert int/slice mix to a tuple of slices
+        elif isinstance(key, tuple):
+            key = tuple((slice(x) if isinstance(x, (int,list)) else x for x in key))
+        
+        # Size tuple to len==5 by appending slice(None) to the end
+        if len(key) < 5:
+            key = tuple(
+                (key[x] if len(key)>x else slice(None) for x in range(5))
+                )
+        else:
+            tuple(key)
+        
+        # Fill in y,x slice None values with shape of sliced images
+        newXYSlices = []
+        for idx,ii in enumerate(key[-2:]):
+            newSlice = []
+            if ii.start is None:
+                newSlice.append(0)
+            else:
+                newSlice.append(ii.start)
+            
+            if ii.stop is None:
+                newSlice.append(self.meta['resolution{}_shape'.format(res)][idx])
+            else:
+                newSlice.append(ii.stop)
+            
+            if ii.step is None:
+                newSlice.append(1)
+            else:
+                newSlice.append(ii.step)
+                
+            newXYSlices.append(slice(*newSlice))
+
+        
+        key = (*key[:-2],*newXYSlices)
+        
+        print('Out key: {}'.format(key))
+        ## NOTE: At this point, key should always be of len(key)==5 and each
+        ## element should be a slice object.
+        return key,res
+        
     
     def fillSlice(self,key):
         '''
@@ -110,6 +195,10 @@ class weave_read:
                 start = stop
                 stop += 1
             
+            if start is None and stop is None:
+                start = 1
+                stop = 1
+            
             newKey.append(slice(start,stop,step))
         
         return tuple(newKey)
@@ -118,6 +207,9 @@ class weave_read:
         
         shape = self.meta['resolution{}_shape'.format(res)]
         
+        shape = (*self.shape[:-2],*shape)
+        print(shape)
+        print(key)
         for dim,_ in enumerate(key):
             if key[dim].stop > shape[dim]+1:
                 raise ValueError('Stop value of {} for dimension {} is bigger than the size of the array: {}'.format(key[dim].stop,dim,shape[dim]))
@@ -182,41 +274,86 @@ class weave_read:
         
     def readSubSample(self,t,c,z,y,x):
         return tifffile.imread(getFullFilePath(self.location,t,c,z,y,x))
+    
+    def readSubSampleSlice(self,t,c,z,y,x,ySlice,xSlice):
+        # Read by slice 
+        fileName = getFullFilePath(self.location,t,c,z,y,x)
+        with tifffile.imread(fileName, aszarr=True) as store:
+            za = zarr.open(store, mode='r')
+            return za[ySlice, xSlice]
         
     ## Possibly depreciated below
-    def pixelLocation(self,t,c,z,yCord,xCord):
+    def pixelLocation(self,res,t,c,z,yCord,xCord):
         '''
         Take a location in the full resolution dataset and return a filename
         and coordinate for the pixel.
         
         Returns a tuple of (image_filename,y_cordinate,x_cordinate) 
+        
+        One use of this function is to find the boundries of a slice in low res
+        versions.
         '''
         
-        imageY = yCord%self.weaveNumber
-        imageYOffset = yCord//self.weaveNumber
+        weaveNumber = self.weaveNumber - res
         
-        imageX = xCord%self.weaveNumber
-        imageXOffset = xCord//self.weaveNumber
+        imageY = yCord%weaveNumber
+        imageYOffset = yCord//weaveNumber
+        
+        imageX = xCord%weaveNumber
+        imageXOffset = xCord//weaveNumber
         
         return ( getFullFilePath(self.location,t,c,z,imageY,imageX), imageYOffset, imageXOffset )
         
     
     def getSlice(self,res,key):
         
-        resolutionLevelShape = self.meta['resolution{}_shape'.format(res)]
         weaveNumber = self.weaveNumber - res
+        print(weaveNumber)
         
         outIter = tuple((range(x.start,x.stop,x.step) for x in key))
+        print(outIter)
         outShape = tuple((len(x) for x in outIter))
         
+        
+        
+        # Determine the specific regions of low res images that must be read
+        
+        # Start values
+        
+        
+        _,yStart,xStart = self.pixelLocation(res,0,0,0,outIter[-2][0],outIter[-1][0])
+        _,yStop,xStop = self.pixelLocation(res,0,0,0,outIter[-2][-1],outIter[-1][-1])
+        yStop += 1
+        xStop += 1
+        
+        lowResYSlice = slice(yStart,yStop,outIter[-2].step)
+        lowResXSlice = slice(xStart,xStop,outIter[-1].step)
+        print(lowResYSlice)
+        print(lowResXSlice)
+        
+        # Form canvas that weave data will be laid onto   
         canvas = np.zeros(outShape,dtype=self.dtype)
-        
-        for tt,cc,zz,yy,xx in product(*outIter):
-            '''
-            Read images off disk and build canvas
-            '''
-        
-        
+        print(canvas.shape)
+        for idxt, tt in enumerate(outIter[0]):
+            for idxc, cc in enumerate(outIter[1]):
+                for idxz, zz in enumerate(outIter[2]):
+                    for ii,oo in product(range(weaveNumber),range(weaveNumber)):
+                        '''Read images off disk and build canvas'''
+                        
+                        ''' ***Need to parallelize the reads*** '''
+                        
+                        '''
+                        The following code (yLen/xLen) trims images read off disk to fit the
+                        canvas slice.  This is necessary because some images are 
+                        smaller by 1 row/col.  Maybe future implementation should
+                        save smaller images padded with a row/col with black to 
+                        make this unnecessary
+                        '''
+                        yLen = len(range(outShape[-2])[ii::weaveNumber])
+                        xLen = len(range(outShape[-1])[oo::weaveNumber])
+                        print('{}_{}_{}_{}_{}'.format(tt,cc,zz,ii,oo))
+                        canvas[idxt,idxc,idxz,ii::weaveNumber,oo::weaveNumber] =\
+                            self.readSubSampleSlice(tt,cc,zz,ii,oo,lowResYSlice,lowResXSlice)[0:yLen,0:xLen]
         
         
         ###############################################################################
@@ -243,27 +380,27 @@ class weave_read:
         #         da.from_delayed(delayed(self.readSubSample)(t,c,z,ii,oo),shape=(y,x),dtype=self.dtype)
         # return canvas.compute()
         
-        ## PARALLEL READS, RAM INEFFICIENT
-        print('Making Canvas')
-        canvas = np.zeros(self.meta['resolution{}_shape'.format(resolutionLevel)],dtype=self.dtype)
-        print('Reading Images')
-        images = (
-            delayed(self.readSubSample)(t,c,z,ii,oo) 
-            for ii,oo in 
-            product(range(weaveNumber),range(weaveNumber))
-            )
-        images = dask.compute(images)[0]
-        # print(images)
-        # print(len(images))
+        # ## PARALLEL READS, RAM INEFFICIENT
+        # print('Making Canvas')
+        # canvas = np.zeros(self.meta['resolution{}_shape'.format(resolutionLevel)],dtype=self.dtype)
+        # print('Reading Images')
+        # images = (
+        #     delayed(self.readSubSample)(t,c,z,ii,oo) 
+        #     for ii,oo in 
+        #     product(range(weaveNumber),range(weaveNumber))
+        #     )
+        # images = dask.compute(images)[0]
+        # # print(images)
+        # # print(len(images))
         
-        # Can this be paralleled?
-        print('Laying Image on Canvas')
-        idx=0
-        for ii,oo in product(range(weaveNumber),range(weaveNumber)):
-            canvas[ii::weaveNumber,oo::weaveNumber] = images[idx]
-            idx+=1
+        # # Can this be paralleled?
+        # print('Laying Image on Canvas')
+        # idx=0
+        # for ii,oo in product(range(weaveNumber),range(weaveNumber)):
+        #     canvas[ii::weaveNumber,oo::weaveNumber] = images[idx]
+        #     idx+=1
                 
-        return canvas
+        return np.squeeze(canvas)
 # a = weave_read(location)
 # start = time.time();z=a[0];print(time.time()-start)
 
